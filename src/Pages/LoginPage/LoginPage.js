c075057e5ae2ce242d4419a4d61c0b93e5125cc1
Bia Luz <anabluz08@gmail.com>
2023-03-16 15:55:22 -0300

    First Commit

---
+++ src/Pages/LoginPage/LoginPage.js
@@ -0,0 +1,12 @@
+import styled from "styled-components"
+
+export default function Login(){ 
+   return( <Tela>login</Tela>);
+
+}
+
+
+const Tela =styled.div `
+width: 375px;
+height: 667px;
+background: #FFFFFF;`
